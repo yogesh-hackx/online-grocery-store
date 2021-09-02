9ad454fe4dcf625504108be3a322df3e5377f177
Yogesh Bhatt <me@yogicodes.com>
2021-09-02 14:08:57 +0530

    update: schema and dir structure

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoose_fuzzy_searching = require('mongoose-fuzzy-searching');
 
 const REQUIRED_STRING = {
     type: String,
@@ -12,30 +13,40 @@ const REQUIRED_NUM = {
 
 const productSchema = new mongoose.Schema({
     name: REQUIRED_STRING,
-    MRP: REQUIRED_NUM,
-    sellingPrice: REQUIRED_NUM,
-    quantityAvailable: {
-        type: [String],
-        required: true,
-    },
+    MRP: REQUIRED_STRING,
+    sellingPrice: REQUIRED_STRING,
     tags: {
         type: [String],
         required: true,
     },
-    productDetails: {
-        nutrientValue: String,
-        shelfLife: String,
-        storageTips: String,
-        seller: {
-            name: REQUIRED_STRING,
-            description: String,
-        },
-        description: REQUIRED_STRING,
-        images: {
-            mainImg: String,
-            extraImages: [String],
-        },
+    category: REQUIRED_STRING,
+    volume: REQUIRED_STRING,
+    imgURL: REQUIRED_STRING,
+    imgURLThumb: REQUIRED_STRING,
+    extraImages: {
+        type: [String],
+        required: true,
     },
+    sku: { ...REQUIRED_NUM, index: true },
+    slug: REQUIRED_STRING,
+
+});
+
+productSchema.plugin(mongoose_fuzzy_searching, {
+    fields: [
+        {
+            name: 'name',
+            weight: 5,
+        },
+        {
+            name: 'tags',
+            weight: 4,
+        },
+        {
+            name: 'category',
+            weight: 3,
+        },
+    ],
 });
 
 const Product = mongoose.model('Product', productSchema);
